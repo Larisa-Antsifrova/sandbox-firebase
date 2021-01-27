fdfbe5c46a308bfab8a1d7bd5a13fb68bfc85214
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-01-27 12:50:27 +0200

    Added delete ui.

--- js/app.js
+++ js/app.js
@@ -6,15 +6,26 @@ function renderCafe(doc) {
   let li = document.createElement("li");
   let name = document.createElement("span");
   let city = document.createElement("span");
+  let remove = document.createElement("i");
 
   li.setAttribute("data-id", doc.id);
   name.textContent = doc.data().name;
   city.textContent = doc.data().city;
+  remove.classList.add("material-icons");
+  remove.innerHTML = "delete_forever";
 
   li.appendChild(name);
   li.appendChild(city);
+  li.appendChild(remove);
 
   cafeList.appendChild(li);
+
+  // deleting data
+  remove.addEventListener("click", (e) => {
+    let id = e.target.parentElement.getAttribute("data-id");
+
+    db.collection("cafes").doc(id).delete();
+  });
 }
 
 //getting data from db
